049f26943217e7832f5a9fe56f703b26a370ae28
Shingo Takemoto <takemoto.s.e908@m.isct.ac.jp>
2025-09-13 15:40:43 +0900

    Update noise conditions for OJSP

--- methods/select_func_GeoSSTV_for_denoising.m
+++ methods/select_func_GeoSSTV_for_denoising.m
@@ -1,22 +1,21 @@
 function [HSI_restored, removed_noise, other_result] ...
-     = select_func_SSTV_for_denoising(HSI_clean, HSI_noisy, params, deg)
+     = select_func_GeoSSTV_for_denoising(HSI_clean, HSI_noisy, params, deg)
+addpath('./methods/GeoSSTV/')
 
-addpath('./methods/SSTV');
-
-% Selecting SSTV function based on noise conditions
+% Selecting RISSTV function based on noise conditions
 if deg.sparse_rate == 0 && deg.stripe_rate == 0 && deg.deadline_rate == 0
     [HSI_restored, removed_noise, other_result] = ...
-        func_SSTV_g_for_denoising(HSI_clean, HSI_noisy, params);
+        func_GeoSSTV_g_for_denoising(HSI_clean, HSI_noisy, params);
 
 elseif deg.stripe_rate == 0
     [HSI_restored, removed_noise, other_result] = ...
-        func_SSTV_gs_for_denoising(HSI_clean, HSI_noisy, params);
+        func_GeoSSTV_gs_for_denoising(HSI_clean, HSI_noisy, params);
 
 elseif deg.sparse_rate == 0 && deg.deadline_rate == 0
     [HSI_restored, removed_noise, other_result] = ...
-        func_SSTV_gt_for_denoising(HSI_clean, HSI_noisy, params);
+        func_GeoSSTV_gt_for_denoising(HSI_clean, HSI_noisy, params);
 
 else
     [HSI_restored, removed_noise, other_result] = ...
-        func_SSTV_gst_for_denoising(HSI_clean, HSI_noisy, params);
+        func_GeoSSTV_gst_for_denoising(HSI_clean, HSI_noisy, params);
 end
--- methods/select_func_l0l1HTV_for_denoising.m
+++ methods/select_func_l0l1HTV_for_denoising.m
@@ -4,7 +4,7 @@ function [HSI_restored, removed_noise, other_result] ...
 addpath('./methods/l0l1HTV');
 
 % Selecting l0l1HTV function based on noise conditions
-if deg.sparse_rate == 0 && deg.stripe_rate == 0
+if deg.sparse_rate == 0 && deg.stripe_rate == 0 && deg.deadline_rate == 0
     [HSI_restored, removed_noise, other_result] = ...
         func_l0l1HTV_g_for_denoising(HSI_clean, HSI_noisy, params);
 
@@ -12,7 +12,7 @@ elseif deg.stripe_rate == 0
     [HSI_restored, removed_noise, other_result] = ...
         func_l0l1HTV_gs_for_denoising(HSI_clean, HSI_noisy, params);
 
-elseif deg.sparse_rate == 0
+elseif deg.sparse_rate == 0 && deg.deadline_rate == 0
     [HSI_restored, removed_noise, other_result] = ...
         func_l0l1HTV_gt_for_denoising(HSI_clean, HSI_noisy, params);
 
