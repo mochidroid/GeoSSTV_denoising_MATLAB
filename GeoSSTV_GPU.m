92573d9745f17274baecd1c91602d40273b7faf7
Shingo Takemoto <takemoto.s.e908@m.isct.ac.jp>
2025-09-29 20:31:26 +0900

    Generate GeoSSTV functions for public

---
+++ GeoSSTV_GPU.m
@@ -0,0 +1,224 @@
+%% Geometric Spatio-Spectral Total Variation for Hyperspectral Image Denoising and Destriping
+%% =========================== First part notes===========================
+% Author: Shingo Takemoto (takemoto.s.e908@m.isct.ac.jp)
+% Last version: Sep. 29, 2025
+% Article: S. Takemoto, S. Ono, 
+%   ``Geometric Spatio-Spectral Total Variation for Hyperspectral Image Denoising and Destriping''
+% -------------------------------------------------------------------------
+%% =========================== Second part notes =========================== 
+% INPUT:
+%   HSI_noisy: noisy hyperspectral image of size n1*n2*n3 normalized to [0,1]
+%   params: an option structure whose fields are as follows:           
+%       alpha: radius of l_1 ball for sparse noise
+%       beta: radius of l_1 ball for stripe noise
+%       epsilon: radius of l_2 ball serving data-fidelity
+%       blocksize: parameter of block size for spatio-spectral structure tensor
+%       max_iter: maximum number of iterations
+%       stop_cri: stopping criterion of P-PDS
+%       disprate: Period to display intermediate results
+% OUTPUT:
+%   restored_HSI: denoised hyperspectral image
+%   removed_noise: removed noise
+%   iteration: number of P-PDS iteration
+%  ========================================================================
+
+function [HSI_restored, removed_noise, iteration, converge_rate_U] ...
+     = GeoSSTV_GPU(HSI_noisy, params)
+fprintf('** Running GeoSSTV_GPU **\n');
+HSI_noisy = gpuArray(single(HSI_noisy));
+[n1, n2, n3] = size(HSI_noisy);
+
+epsilon     = gpuArray(single(params.epsilon));
+alpha       = gpuArray(single(params.alpha));
+beta        = gpuArray(single(params.beta));
+lambda      = gpuArray(single(params.lambda));
+maxiter     = gpuArray(single(params.maxiter));
+stopcri     = gpuArray(single(params.stopcri));
+
+%% Setting params
+dispiter    = unique([1:10, 1000:1000:maxiter]);
+dispband    = round(n3/2);
+
+
+%% Initializing primal and dual variables
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% primal variables
+% U: clean HSI
+% W1: interpolation (first-order difference)
+% W2: interpolation (second-order difference)
+% S: sparse noise(salt-and-pepper noise)
+% T: stripe noise
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+U = zeros([n1, n2, n3], 'single', 'gpuArray');
+W1 = zeros([n1, n2, n3, 2, 3], "single", "gpuArray");
+W2 = zeros([n1, n2, n3, 2, 3], "single", "gpuArray");
+S = zeros([n1, n2, n3], 'single', 'gpuArray');
+T = zeros([n1, n2, n3], 'single', 'gpuArray');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% dual variables
+% Y1: term of connection with Du and L*w
+% Y2: term of connection with DDsu and L*w 
+% Y2: term of l2ball
+% Y3: term of stripe noise
+%
+% Y1 = D(U) - L*(W1)
+% Y2 = D(Ds(U)) - L*(W2) 
+% Y3 = U + S + T
+% Y4 = Dv(T)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Y1 = zeros([n1, n2, n3, 2], "single", "gpuArray");
+Y2 = zeros([n1, n2, n3, 2], "single", "gpuArray");
+Y3 = zeros([n1, n2, n3], 'single', 'gpuArray');
+Y4 = zeros([n1, n2, n3], 'single', 'gpuArray');
+
+
+%% Setting operators
+% Difference operators with Neumann boundary
+D       = @(z) cat(4, z([2:end, end],:,:) - z, z(:,[2:end, end],:) - z);
+Dt      = @(z) cat(1, -z(1, :, :, 1), -z(2:end-1, :, :, 1) + z(1:end-2, :, :, 1), z(end-1, :, :, 1)) ...
+        + cat(2, -z(:, 1, :, 2), -z(:, 2:end-1, :, 2) + z(:, 1:end-2, :, 2), z(:, end-1, :, 2));
+
+Dl      = @(z) z(:, :, [2:end, end]) - z;
+Dlt     = @(z) cat(3, -z(:, :, 1), -z(:, :, 2:end-1) + z(:, :, 1:end-2), z(:, :, end-1));
+
+Dv      = @(z) z([2:end, end], :, :) - z;
+Dvt     = @(z) cat(1, -z(1, :, :), -z(2:(end-1), :, :) + z(1:(end-2), :, :), z(end-1, :, :));
+
+
+%% Setting step size for P-PDS
+gamma1_U    = gpuArray(single(1/(8 + 8*4 + 1)));
+gamma1_W1   = gpuArray(single(1/4));
+gamma1_W2   = gpuArray(single(1/4));
+gamma1_S    = gpuArray(single(1));
+gamma1_T    = gpuArray(single(1/(4 + 1)));
+gamma2      = gpuArray(single(1/5));
+
+
+%% main loop (P-PDS)
+converge_rate_U = zeros([1, maxiter], 'single');
+move_function_value = zeros([1, maxiter], "single");
+fprintf('~~~ P-PDS STARTS ~~~\n');
+
+for i = 1:maxiter   
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Updating U
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    U_tmp   = U - gamma1_U*(Dt(Y1) + Dlt(Dt(Y2)) + Y3);
+    U_next  = ProjBox(U_tmp, 0, 1);
+    U_res   = 2*U_next - U;
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Updating S
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    S_tmp   = S - gamma1_S.*Y3;
+    S_next  = ProjFastL1Ball(S_tmp, alpha);
+    S_res = 2*S_next - S;
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Updating T
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    T_tmp   = T - gamma1_T.*(Y3 + Dvt(Y4));
+    T_next  = ProjFastL1Ball(T_tmp, beta);
+    T_res = 2*T_next - T;
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Updating W1
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    W1_tmp   = W1 + gamma1_W1*(opL(Y1));
+    W1_next  = Prox_l12norm_d4(W1_tmp, lambda*gamma1_W1);
+    W1_res   = 2*W1_next - W1;
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Updating W2
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    W2_tmp   = W2 + gamma1_W2*(opL(Y2));
+    W2_next  = Prox_l12norm_d4(W2_tmp, gamma1_W2);
+    W2_res   = 2*W2_next - W2;
+
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Updating Y1
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    Y1_next  = Y1 + gamma2*(D(U_res)-opLadj(W1_res));
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Updating Y2
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    Y2_next  = Y2 + gamma2*(D(Dl(U_res))-opLadj(W2_res));
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Updating Y3
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    Y3_tmp  = Y3 + gamma2*(U_res + S_res + T_res);    
+    Y3_next = Y3_tmp - gamma2*ProjL2ball(Y3_tmp./gamma2_Y2, HSI_noisy, epsilon);
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Updating Y4
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    Y4_next = Y4 + gamma2*Dv(T_res);
+
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Calculating error
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    converge_rate_U(i) = norm(U_next(:) - U(:),2)/norm(U(:),2);
+    move_function_value(i) = lambda*sum(sqrt(sum(W1.^2,4)), "all") + sum(sqrt(sum(W2.^2,4)), "all");
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Updating all variables
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    U   = U_next;
+    S   = S_next;
+    T   = T_next;
+    
+    Y1  = Y1_next;
+    Y2  = Y2_next;
+    Y3  = Y3_next;
+    Y4  = Y4_next;
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Convergence checking
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    if i>=2 && converge_rate_U(i) < stopcri % Checking convergence condition
+        fprintf('Iter: %d, Error: %0.6f, Function value: %#.4g.\n', ...
+            i, converge_rate_U(i), move_function_value(i));
+        break
+    end
+    if (mod(i, disprate) == 0) % Displaying intermediate results
+        fprintf('Iter: %d, Error: %0.6f, Function value: %#.4g.\n', ...
+            i, converge_rate_U(i), move_function_value(i));
+        figure(1)
+            subplot(2,2,1)
+            imshow(HSI_noisy(:,:,dispband));
+            title("Observed")
+            
+            subplot(2,2,2)
+            imshow(gather(U(:,:,dispband)));
+            title("Restored")
+    
+            subplot(2,2,3)
+            semilogy(converge_rate_U(1:i));
+            title("Converge rate U TV")
+            
+            subplot(2,2,4)
+            semilogy(move_function_value(1:i));
+            title("Function value")
+            drawnow
+    end
+end
+
+fprintf('~~~ P-PDS ENDS ~~~\n');
+
+%% Organizing results for output
+HSI_noisy                       = gather(HSI_noisy);
+HSI_restored                    = gather(U);
+iteration                       = gather(i);
+removed_noise.sparse_noise      = gather(S);
+removed_noise.stripe_noise      = gather(T);
+removed_noise.gaussian_noise    = HSI_noisy - HSI_restored - ...
+                                    removed_noise.sparse_noise - removed_noise.stripe_noise;
+converge_rate_U                 = gather(converge_rate_U(1:iteration));
---
+++ main_GeoSSTV.m
@@ -0,0 +1,95 @@
+%% Geometric Spatio-Spectral Total Variation for Hyperspectral Image Denoising and Destriping
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Author: Shingo Takemoto (takemoto.s.e908@m.isct.ac.jp)
+% Last version: Sep. 29, 2025
+% Article: S. Takemoto, S. Ono, 
+%   ``Geometric Spatio-Spectral Total Variation for Hyperspectral Image Denoising and Destriping''
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear
+close all
+addpath(genpath('sub_functions'))
+fprintf('******* initium *******\n');
+rng('default')
+
+%% Generating observation
+%%%%%%%%%%%%%%%%%%%%% User settings of experiment %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+deg.Gaussian_sigma      = 0.1; % Standard derivation of Gaussian noise
+deg.sparse_rate         = 0.05; % Rate of sparse noise
+deg.stripe_rate         = 0.05; % Rate of stripe noise
+deg.stripe_intensity    = 0.5; % Range of intensity for stripe noise
+deg.deadline_rate       = 0.01; % Rate of deadline noise
+
+image = 'JasperRidge';
+% image = 'PaviaUniversity';
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[HSI_clean] = Load_HSI(image);
+[HSI_noisy, deg] = Generate_obsv(HSI_clean, deg);
+
+HSI_clean = single(HSI_clean);
+HSI_noisy = single(HSI_noisy);
+
+
+%% Setting parameters
+%%%%%%%%%%%%%%%%%%%%% User Settings %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+params.rho = 0.95; % Parameter for the radii of the noise constraints
+
+params.omega = 0.03; % Balancing parameter between first and second-order differences
+
+params.stopcri = 1e-5; % Stopping criterion
+params.maxiter = 20000; % Maximum number of iterations
+
+use_GPU = 1; % 1, if you use GPU, 0, if you use CPU
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Radius of v-centered l2-ball constraint serving data-fidelity
+params.epsilon = params.rho * deg.Gaussian_sigma * sqrt(hsi.N * (1 - deg.sparse_rate));
+
+% Radius of l1-ball constraint for sparse noise
+params.alpha = params.rho * (0.5 * hsi.N * deg.sparse_rate);
+
+% Radius of l1-ball constraint for stripe noise
+params.beta = params.rho * hsi.N * (1 - deg.sparse_rate) ...
+    * deg.stripe_rate * deg.stripe_intensity / 2; 
+
+
+%% Showing settings
+fprintf('~~~ SETTINGS ~~~\n');
+fprintf('Image: %s Size: (%d, %d, %d)\n', image, hsi.n1, hsi.n2, hsi.n3);
+fprintf('Gaussian sigma: %g\n', deg.Gaussian_sigma);
+fprintf('Sparse rate: %g\n', deg.sparse_rate);
+fprintf('Stripe rate: %g\n', deg.stripe_rate);
+fprintf('Stripe intensity: %g\n', deg.stripe_intensity);
+fprintf('Rho: %g\n', params.rho);
+fprintf('Omega: %g\n', params.omega);
+fprintf('Stopping criterion: 1e-%d\n', params.stopcri);
+
+
+%% Denoising and destriping
+if use_GPU == 1
+    [HSI_restored, ~, ~, ~] = S3TTV_GPU(HSI_noisy, params); % for GPU user
+elseif use_GPU == 0
+    [HSI_restored, ~, ~, ~] = S3TTV_CPU(HSI_noisy, params); % for CPU user
+else
+end
+
+
+%% Plotting results
+val_mpsnr  = calc_MPSNR(HSI_restored(:, :, edge_width+1:end-edge_width), HSI_clean(:, :, edge_width+1:end-edge_width));
+val_mssim  = calc_MSSIM(HSI_restored(:, :, edge_width+1:end-edge_width), HSI_clean(:, :, edge_width+1:end-edge_width));
+
+fprintf('~~~ RESULTS ~~~\n');
+fprintf('MPSNR: %#.4g\n', val_mpsnr);
+fprintf('MSSIM: %#.4g\n', val_mssim);
+
+
+%% Showing result images
+visband = 59; % band to visualize
+
+figure(1);
+subplot(1,3,1), imshow(HSI_clean(:,:,visband)), title('Ground-truth');
+subplot(1,3,2), imshow(HSI_noisy(:,:,visband)), title('Noisy HSI');
+subplot(1,3,3), imshow(HSI_restored(:,:,visband)), title('Restored HSI');
+
+fprintf('******* finis *******\n');
